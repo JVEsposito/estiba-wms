06daaeb51bfded0684b424a4b39699c7767b3016
Leskaos <136376805+Leskaos@users.noreply.github.com>
2026-07-17 13:16:44 -0400

    feat: registrar rutas de validacion

--- routes/api.php
+++ routes/api.php
@@ -7,51 +7,51 @@ use App\Http\Controllers\Api\AndenController;
 use App\Http\Controllers\Api\CamaraController;
 use App\Http\Controllers\Api\CargaController;
 use App\Http\Controllers\Api\CatalogoMaterialController;
+use App\Http\Controllers\Api\CatalogoValidacionController;
 use App\Http\Controllers\Api\CondicionSagController;
 use App\Http\Controllers\Api\ConfiguracionCamaraController;
 use App\Http\Controllers\Api\DespachoFrigorificoController;
 use App\Http\Controllers\Api\DespachoMaterialController;
 use App\Http\Controllers\Api\MovimientoController;
 use App\Http\Controllers\Api\SesionEstibaController;
+use App\Http\Controllers\Api\ValidacionPalletController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/acceso-tablet', [AccesoTabletController::class, 'store'])
-    ->middleware('throttle:6,1');
-Route::post('/acceso-oficina', [AccesoOficinaController::class, 'store'])
-    ->middleware('throttle:6,1');
+Route::post('/acceso-tablet', [AccesoTabletController::class, 'store'])->middleware('throttle:6,1');
+Route::post('/acceso-oficina', [AccesoOficinaController::class, 'store'])->middleware('throttle:6,1');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+    Route::get('/user', fn (Request $request) => $request->user());
 
     Route::get('/camaras', [CamaraController::class, 'index']);
     Route::get('/camaras/{camara}/plano', [CamaraController::class, 'plano']);
     Route::get('/condiciones-sag', [CondicionSagController::class, 'index']);
+
+    Route::get('/validacion/catalogos', CatalogoValidacionController::class)->middleware('can:validar-pallets');
+    Route::post('/validacion/pallets', [ValidacionPalletController::class, 'store'])->middleware('can:validar-pallets');
+    Route::middleware('can:consultar-validaciones-pallet')->group(function () {
+        Route::get('/validacion/pallets', [ValidacionPalletController::class, 'index']);
+        Route::get('/validacion/pallets/{validacionPallet}', [ValidacionPalletController::class, 'show']);
+    });
+
     Route::middleware('can:consultar-despachos-materiales')->group(function () {
         Route::get('/materiales/catalogo', [CatalogoMaterialController::class, 'catalogo']);
         Route::get('/materiales/inventario', [DespachoMaterialController::class, 'inventario']);
         Route::get('/materiales/despachos', [DespachoMaterialController::class, 'index']);
         Route::get('/materiales/despachos/{despachoMaterial}', [DespachoMaterialController::class, 'show']);
     });
-    Route::get('/materiales/kardex', [DespachoMaterialController::class, 'kardex'])
-        ->middleware('can:consultar-kardex-materiales');
-    Route::middleware('can:gestionar-despachos-materiales')->group(function () {
-        Route::post('/materiales/despachos', [DespachoMaterialController::class, 'store']);
-    });
-    Route::post('/materiales/despachos/{despachoMaterial}/retirar', [DespachoMaterialController::class, 'retirar'])
-        ->middleware('can:retirar-materiales');
-    Route::post('/materiales/despachos/{despachoMaterial}/cancelar', [DespachoMaterialController::class, 'cancelar'])
-        ->middleware('can:cancelar-despachos-materiales');
+    Route::get('/materiales/kardex', [DespachoMaterialController::class, 'kardex'])->middleware('can:consultar-kardex-materiales');
+    Route::post('/materiales/despachos', [DespachoMaterialController::class, 'store'])->middleware('can:gestionar-despachos-materiales');
+    Route::post('/materiales/despachos/{despachoMaterial}/retirar', [DespachoMaterialController::class, 'retirar'])->middleware('can:retirar-materiales');
+    Route::post('/materiales/despachos/{despachoMaterial}/cancelar', [DespachoMaterialController::class, 'cancelar'])->middleware('can:cancelar-despachos-materiales');
 
     Route::middleware('can:consultar-cargas-operacion')->group(function () {
         Route::get('/cargas/pendientes', [CargaController::class, 'pendientes']);
         Route::get('/cargas/{carga}/tareas', [DespachoFrigorificoController::class, 'tareas']);
         Route::get('/andenes', [AndenController::class, 'index']);
     });
-    Route::get('/cargas/folios-disponibles', [CargaController::class, 'foliosDisponibles'])
-        ->middleware('can:gestionar-cargas');
+    Route::get('/cargas/folios-disponibles', [CargaController::class, 'foliosDisponibles'])->middleware('can:gestionar-cargas');
     Route::middleware('can:consultar-catalogo-cargas')->group(function () {
         Route::get('/cargas/incidencias', [DespachoFrigorificoController::class, 'incidencias']);
         Route::get('/cargas', [CargaController::class, 'index']);
@@ -76,10 +76,8 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('/configuracion/camaras/{camara}', [ConfiguracionCamaraController::class, 'show']);
     });
     Route::post('/configuracion/camaras', [ConfiguracionCamaraController::class, 'store']);
-    Route::put('/configuracion/camaras/{camara}', [ConfiguracionCamaraController::class, 'update'])
-        ->middleware('can:administrar-camaras');
-    Route::delete('/configuracion/camaras/{camara}', [ConfiguracionCamaraController::class, 'destroy'])
-        ->middleware('can:administrar-camaras');
+    Route::put('/configuracion/camaras/{camara}', [ConfiguracionCamaraController::class, 'update'])->middleware('can:administrar-camaras');
+    Route::delete('/configuracion/camaras/{camara}', [ConfiguracionCamaraController::class, 'destroy'])->middleware('can:administrar-camaras');
 
     Route::middleware('can:administrar-accesos')->group(function () {
         Route::get('/administracion/accesos', [AdministracionAccesoController::class, 'index']);
@@ -98,16 +96,14 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/administracion/materiales/destinos', [CatalogoMaterialController::class, 'storeDestino']);
         Route::put('/administracion/materiales/destinos/{destinoMaterial}', [CatalogoMaterialController::class, 'updateDestino']);
     });
+
     Route::post('/camaras/{camara}/sesiones', [SesionEstibaController::class, 'store']);
     Route::post('/sesiones/{sesion}/cerrar', [SesionEstibaController::class, 'cerrar']);
     Route::post('/sesiones/{sesion}/cerrar-forzosamente', [SesionEstibaController::class, 'cerrarForzosamente']);
-
     Route::get('/movimientos/recientes', [MovimientoController::class, 'recientes']);
     Route::post('/movimientos/ubicar', [MovimientoController::class, 'ubicar']);
     Route::post('/movimientos/mover', [MovimientoController::class, 'mover']);
 
-    Route::delete('/acceso-tablet', [AccesoTabletController::class, 'destroy'])
-        ->middleware('auth:sanctum');
-    Route::delete('/acceso-oficina', [AccesoOficinaController::class, 'destroy'])
-        ->middleware('auth:sanctum');
+    Route::delete('/acceso-tablet', [AccesoTabletController::class, 'destroy']);
+    Route::delete('/acceso-oficina', [AccesoOficinaController::class, 'destroy']);
 });
