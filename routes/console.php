77ae57f01c523c9f96c5813eebfa9b02de97ef92
JVEsposito <134811446+JVEsposito@users.noreply.github.com>
2026-07-21 10:31:54 -0400

    style: ajustar import de Telescope

--- routes/console.php
+++ routes/console.php
@@ -3,12 +3,13 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use Laravel\Telescope\TelescopeServiceProvider;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
 if (app()->environment('local') &&
-    class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
+    class_exists(TelescopeServiceProvider::class)) {
     Schedule::command('telescope:prune --hours=48')->dailyAt('02:00');
 }
