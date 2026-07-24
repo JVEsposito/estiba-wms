b0be762d623690d8dda1500d6f31e8c61720bd7b
JVEsposito <134811446+JVEsposito@users.noreply.github.com>
2026-07-24 13:53:40 -0400

    Retirar proveedores dedicados a rutas API

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,12 +2,8 @@
 
 use App\Providers\AdministracionUsuarioServiceProvider;
 use App\Providers\AppServiceProvider;
-use App\Providers\RecepcionMaterialServiceProvider;
-use App\Providers\TransformacionMaterialServiceProvider;
 
 return [
     AppServiceProvider::class,
     AdministracionUsuarioServiceProvider::class,
-    RecepcionMaterialServiceProvider::class,
-    TransformacionMaterialServiceProvider::class,
 ];
