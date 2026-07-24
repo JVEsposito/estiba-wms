01207372d4a278c3291a5aa799c35ecc00dbf364
Leskaos <136376805+Leskaos@users.noreply.github.com>
2026-07-24 09:24:41 -0400

    feat: agregar movimientos de transformación

--- app/Enums/TipoMovimientoInventarioMaterial.php
+++ app/Enums/TipoMovimientoInventarioMaterial.php
@@ -12,4 +12,8 @@ enum TipoMovimientoInventarioMaterial: string
     case Devolucion = 'devolucion';
     case CorreccionItemSalida = 'correccion_item_salida';
     case CorreccionItemEntrada = 'correccion_item_entrada';
+    case ConsumoTransformacion = 'consumo_transformacion';
+    case ProduccionTransformacion = 'produccion_transformacion';
+    case MermaTransformacion = 'merma_transformacion';
+    case ReversaTransformacion = 'reversa_transformacion';
 }
