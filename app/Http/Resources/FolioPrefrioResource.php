71c99e0835076355e84493b2ae3fee6a3f6cd2c5
Leskaos <136376805+Leskaos@users.noreply.github.com>
2026-07-27 12:54:50 -0400

    feat: incluir trazabilidad de validación en prefrío

--- app/Http/Resources/FolioPrefrioResource.php
+++ app/Http/Resources/FolioPrefrioResource.php
@@ -12,6 +12,8 @@ class FolioPrefrioResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $datosExternos = is_array($this->datos_externos) ? $this->datos_externos : [];
+
         return [
             'id' => $this->id,
             'numero_folio' => $this->numero_folio,
@@ -19,10 +21,18 @@ class FolioPrefrioResource extends JsonResource
             'estado_operacional' => $this->estado_operacional->value,
             'condicion_termica' => $this->condicion_termica?->value,
             'habilitacion_almacenamiento' => $this->habilitacion_almacenamiento?->value,
+            'especie' => $datosExternos['especie'] ?? null,
             'variedad' => $this->variedad,
             'calibre' => $this->calibre,
+            'envase' => $datosExternos['envase'] ?? null,
+            'categoria' => $datosExternos['categoria'] ?? null,
             'marca' => $this->marca,
             'exportadora' => $this->exportadora,
+            'csg' => $datosExternos['csg'] ?? null,
+            'predio' => $datosExternos['predio'] ?? null,
+            'cantidad_cajas' => isset($datosExternos['cantidad_cajas'])
+                ? (int) $datosExternos['cantidad_cajas']
+                : null,
             'fecha_ingreso' => $this->fecha_ingreso?->toAtomString(),
         ];
     }
