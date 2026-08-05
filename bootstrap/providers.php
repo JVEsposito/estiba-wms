df8045b004c7774fd4dfd5ab1b3e4ebaba15e55d
Leskaos <136376805+Leskaos@users.noreply.github.com>
2026-08-05 11:58:01 -0400

    refactor(materiales): retirar proveedor dedicado a rutas

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,11 +3,9 @@
 use App\Providers\AdministracionUsuarioServiceProvider;
 use App\Providers\AppServiceProvider;
 use App\Providers\CustodiaDistribuidaMaterialesServiceProvider;
-use App\Providers\ImportacionRecepcionesMaterialesServiceProvider;
 
 return [
     AppServiceProvider::class,
     AdministracionUsuarioServiceProvider::class,
     CustodiaDistribuidaMaterialesServiceProvider::class,
-    ImportacionRecepcionesMaterialesServiceProvider::class,
 ];
