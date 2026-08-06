af9cbd4f7564a0a52815ee27c8b899ef562581a1
Leskaos <136376805+Leskaos@users.noreply.github.com>
2026-08-06 13:28:50 -0400

    feat(repaletizajes): habilitar proveedor del módulo

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,9 +3,11 @@
 use App\Providers\AdministracionUsuarioServiceProvider;
 use App\Providers\AppServiceProvider;
 use App\Providers\CustodiaDistribuidaMaterialesServiceProvider;
+use App\Providers\RepaletizajeServiceProvider;
 
 return [
     AppServiceProvider::class,
     AdministracionUsuarioServiceProvider::class,
     CustodiaDistribuidaMaterialesServiceProvider::class,
+    RepaletizajeServiceProvider::class,
 ];
