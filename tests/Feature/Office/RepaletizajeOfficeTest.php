e983aa400600316fa395dbdab88f285d2a1aa1fd
Leskaos <136376805+Leskaos@users.noreply.github.com>
2026-08-06 13:44:07 -0400

    test(repaletizajes): validar visibilidad de oficina

---
+++ tests/Feature/Office/RepaletizajeOfficeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature\Office;
+
+use Tests\TestCase;
+
+class RepaletizajeOfficeTest extends TestCase
+{
+    public function test_la_oficina_de_repaletizajes_es_visible(): void
+    {
+        $this->get('/oficina/validacion/repaletizajes')
+            ->assertOk()
+            ->assertSee('Repaletizajes')
+            ->assertSee('Consolidación de saldos', escape: false);
+    }
+}
