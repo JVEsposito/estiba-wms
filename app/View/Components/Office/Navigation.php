9d53071653ecaf860dbc9bfc97c3983fcd6b36cb
JVEsposito <134811446+JVEsposito@users.noreply.github.com>
2026-08-07 10:30:28 -0400

    fix(navegacion): integrar accesos PT en subnavegacion

--- app/View/Components/Office/Navigation.php
+++ app/View/Components/Office/Navigation.php
@@ -17,47 +17,6 @@ class Navigation extends Component
 
     public function render(): View|Closure|string
     {
-        return function (): string {
-            $html = view('components.office.navigation', [
-                'domain' => $this->domain,
-                'office' => $this->office,
-                'context' => $this->context,
-                'icon' => $this->icon,
-            ])->render();
-
-            if ($this->domain !== 'frigorifico') {
-                return $html;
-            }
-
-            $links = [
-                [
-                    'key' => 'repaletizajes',
-                    'label' => 'Repaletizajes',
-                    'href' => '/oficina/validacion/repaletizajes',
-                ],
-                [
-                    'key' => 'anulaciones-validacion',
-                    'label' => 'Anulaciones',
-                    'href' => '/oficina/validacion/anulaciones',
-                ],
-            ];
-
-            $extras = collect($links)->map(function (array $link): string {
-                $active = $this->office === $link['key'] ? ' class="is-active"' : '';
-
-                return sprintf(
-                    '<a%s data-office-key="%s" data-office-domain="frigorifico" data-navigation-permissions="puede_consultar_validaciones_pallet" data-navigation-module="frigorifico.validacion" href="%s">%s</a>',
-                    $active,
-                    $link['key'],
-                    $link['href'],
-                    $link['label'],
-                );
-            })->implode('');
-            $position = strrpos($html, '</nav>');
-
-            return $position === false
-                ? $html.$extras
-                : substr($html, 0, $position).$extras.substr($html, $position);
-        };
+        return view('components.office.navigation');
     }
 }
