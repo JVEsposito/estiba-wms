aedcde17229a6a944babe17c18439c8b130835f7
Leskaos <136376805+Leskaos@users.noreply.github.com>
2026-08-07 09:22:48 -0400

    test: aislar autenticación al cambiar token

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,12 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    public function withToken($token, $type = 'Bearer')
+    {
+        if (isset($this->app)) {
+            $this->app->make('auth')->forgetGuards();
+        }
+
+        return parent::withToken($token, $type);
+    }
 }
