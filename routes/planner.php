e486e189c6846b58e85e0865b1e18e7b5133363f
Leskaos <136376805+Leskaos@users.noreply.github.com>
2026-09-03 10:40:44 -0400

    Exponer endpoints rolling del planificador

---
+++ routes/planner.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Api\PlanOperacionalController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum', 'can:operar-camaras-productos'])->group(function () {
+    Route::get(
+        '/planes-operacionales/{planOperacional}/snapshot',
+        [PlanOperacionalController::class, 'snapshot'],
+    );
+    Route::post(
+        '/planes-operacionales/{planOperacional}/frontera',
+        [PlanOperacionalController::class, 'materializarFrontera'],
+    );
+    Route::post(
+        '/tareas-movimiento/{tareaMovimiento}/iniciar',
+        [PlanOperacionalController::class, 'iniciar'],
+    );
+});
